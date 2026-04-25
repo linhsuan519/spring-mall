f741e80010f48ff1ee4f232d1af6335e199572dc
jason <s940317@gmail.com>
2026-04-26 00:27:01 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/jason/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/jason/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,15 +1,17 @@
 package com.jason.springbootmall.dao.impl;
 
 import com.jason.springbootmall.dao.ProductDao;
+import com.jason.springbootmall.dto.ProductRequest;
 import com.jason.springbootmall.model.Product;
 import com.jason.springbootmall.rowmapper.ProductRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class ProductDaoImpl implements ProductDao {
@@ -19,7 +21,7 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public Product getProductById(Integer productId) {
-        String sql = "SELECT product_id, product_name, category,image_url, price, stock, description, created_date, last_modified_date " +
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, created_date, last_modified_date " +
                 "FROM product " +
                 "WHERE product_id=:productId";
 
@@ -32,5 +34,30 @@ public class ProductDaoImpl implements ProductDao {
         } else {
             return null;
         }
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+        String sql = "INSERT INTO product(product_name, category, image_url, price, stock, description, created_date, last_modified_date)" +
+                "VALUES(:productName, :category, :imageUrl, :price, :stock, :description, :createdDate, :lastModifiedDate)";
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+
+        Date now = new Date();
+        map.put("createdDate", now);
+        map.put("lastModifiedDate", now);
+        //取得資料庫自動生成流水號
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).intValue();
     }
 }
--- src/main/java/com/jason/springbootmall/service/ProductService.java
+++ src/main/java/com/jason/springbootmall/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.jason.springbootmall.service;
 
+import com.jason.springbootmall.dto.ProductRequest;
 import com.jason.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
-
+    Integer createProduct(ProductRequest productRequest);
 }
