bf4eba72d171044d865da540cf867c2b3fadb093
jason <s940317@gmail.com>
2026-04-28 23:07:11 +0800

    測試

--- src/main/java/com/jason/springbootmall/service/UserService.java
+++ src/main/java/com/jason/springbootmall/service/UserService.java
@@ -6,7 +6,7 @@ import com.jason.springbootmall.model.Product;
 import com.jason.springbootmall.model.User;
 
 public interface UserService {
-
+//
     Integer register(UserRegisterRequest userRegisterRequest);
     User getUserById(Integer userId);
 }
